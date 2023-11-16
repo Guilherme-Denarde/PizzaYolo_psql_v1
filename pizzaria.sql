bfb9fe8513374c8f6abeb2737d90f2952bcaffe9
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2023-11-16 16:26:25 -0300

    Update pizzaria.sql

--- pizzaria.sql
+++ pizzaria.sql
@@ -1,239 +1,250 @@
--- Drop tables in the correct order
-DROP TABLE IF EXISTS public.reviews CASCADE;
-DROP TABLE IF EXISTS public.order_items CASCADE;
-DROP TABLE IF EXISTS public.orders CASCADE;
-DROP TABLE IF EXISTS public.product CASCADE;
-DROP TABLE IF EXISTS public.flavor CASCADE;
-DROP TABLE IF EXISTS public.delivery_people CASCADE;
-DROP TABLE IF EXISTS public.client CASCADE;
-DROP TABLE IF EXISTS public.employ CASCADE;
-DROP TABLE IF EXISTS public.address CASCADE;
-DROP TABLE IF EXISTS public.register_user CASCADE;
-DROP TABLE IF EXISTS public.report CASCADE;
-
--- Drop types
-DROP TYPE IF EXISTS public.permision;
-DROP TYPE IF EXISTS public.payment;
-DROP TYPE IF EXISTS public.order_size;
-DROP TYPE IF EXISTS public.order_state;
-
--- Create ENUM types
-CREATE TYPE public.permision AS ENUM (
-    'administrator',
-    'manager',
-    'employee'
+CREATE TYPE payment_method_type AS ENUM ('PIX', 'CARD', 'MONEY');
+CREATE TYPE roles AS ENUM ('ADMIN', 'USER', 'RESTAURANT');
+CREATE TYPE processing_status AS ENUM ('PROCESSANDO', 'A_CAMINHO', 'ENTREGUE', 'ERRO');
+CREATE TYPE status_type AS ENUM ('ATIVO', 'INATIVO');
+CREATE TYPE vehicle_type AS ENUM ('BICICLETA', 'MOTO', 'CARRO');
+CREATE TYPE day_of_week AS ENUM ('DOMINGO', 'SEGUNDA', 'TERÇA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO');
+CREATE TYPE reviewable_type AS ENUM ('PRODUTO', 'RESTAURANTE');
+CREATE TYPE category_type AS ENUM (
+    'BRASILEIRA', 
+    'ITALIANA', 
+    'JAPONESA', 
+    'CHINESA', 
+    'FAST_FOOD', 
+    'VEGETARIANA', 
+    'VEGANA', 
+    'SOBREMESAS', 
+    'LANCHES', 
+    'PIZZA', 
+    'MARISQUEIRA', 
+    'PADARIA', 
+    'CAFETERIA', 
+    'MEXICANA',
+	'BEBIDA'
 );
 
-CREATE TYPE public.payment AS ENUM (
-    'pix',
-    'card',
-    'money',
-    'check'
+
+CREATE TABLE Role (
+  role_id SERIAL PRIMARY KEY,
+  role_name roles NOT NULL
 );
 
-CREATE TYPE public.order_size AS ENUM (
-    'p',
-    'm',
-    'g',
-    'gg'
+CREATE TABLE Product_Category (
+  id SERIAL PRIMARY KEY,
+  name category_type NOT NULL,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TYPE public.order_state AS ENUM (
-    'open',
-    'making',
-    'finished',
-    'canceled'
+CREATE TABLE Payment_Method (
+  id SERIAL PRIMARY KEY,
+  method_name payment_method_type NOT NULL,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
--- Create tables
-CREATE TABLE public.register_user (
-    user_id serial PRIMARY KEY NOT NULL,
-    name varchar(90) UNIQUE NOT NULL,
-    email varchar(255) UNIQUE NOT NULL, 
-    password varchar(255) NOT NULL,
-    salt varchar(255) NOT NULL,
-    is_active boolean DEFAULT TRUE,
-    last_login timestamp,
-    CONSTRAINT uk_email UNIQUE(email)
+CREATE TABLE Promotion (
+  id SERIAL PRIMARY KEY,
+  description TEXT,
+  discount_rate FLOAT,
+  start_date DATE,
+  end_date DATE,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.address (
-    id serial PRIMARY KEY NOT NULL,
-    streetName varchar(90) NOT NULL,
-    streetNum int NOT NULL,
-    addressReference varchar(90),
-    city varchar(60) NOT NULL,
-    state varchar(60),
-    postal_code varchar(10)
+CREATE TABLE "user" (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone VARCHAR(20),
+  profile_picture VARCHAR(255),
+  date_of_birth DATE,
+  cpf VARCHAR(20),
+  role_id INT REFERENCES Role(role_id),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.employ (
-    id serial PRIMARY KEY NOT NULL,
-    user_id int,
-    cpf varchar(16) UNIQUE NOT NULL,
-    name varchar(90) UNIQUE NOT NULL,
-    phone varchar(15) UNIQUE NOT NULL,
-    permission public.permision,
-    salary int NOT NULL
+CREATE TABLE Address (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES "user"(id),
+  streetName VARCHAR(255) NOT NULL,
+  streetNum INT NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  state VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  zipcode VARCHAR(20),
+  additional_info TEXT,
+  is_default BOOLEAN,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.client (
-    id serial PRIMARY KEY NOT NULL,
-    user_id int,
-    address_id int,
-    cpf varchar(16) UNIQUE NOT NULL,
-    name varchar(90) UNIQUE NOT NULL,
-    phone varchar(15) UNIQUE NOT NULL, 
-    CONSTRAINT fk_register_user
-        FOREIGN KEY (user_id) REFERENCES public.register_user(user_id),
-    CONSTRAINT fk_address
-        FOREIGN KEY (address_id) REFERENCES public.address(id)
+CREATE TABLE Card (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES "user"(id),
+  card_number VARCHAR(255) NOT NULL,
+  cardholder_name VARCHAR(255) NOT NULL,
+  expiry_date DATE,
+  cvv VARCHAR(4),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.delivery_people (
-    id serial PRIMARY KEY NOT NULL,
-    employ_id int,
-    cpf varchar(16) NOT NULL,
-    name varchar(90) NOT NULL,
-    phone varchar(15) NOT NULL, 
-    FOREIGN KEY (employ_id) REFERENCES public.employ(id)
+CREATE TABLE Restaurant (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  address_id INT REFERENCES Address(id),
+  email VARCHAR(255),
+  phone VARCHAR(20),
+  description TEXT,
+  rating_average FLOAT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.flavor (
-    id serial PRIMARY KEY NOT NULL,
-    flavor_name varchar(90) UNIQUE NOT NULL,
-    flavor_price numeric(10, 2) NOT NULL,
-    flavor_ingredients varchar(255)
+CREATE TABLE Restaurant_Category (
+  id SERIAL PRIMARY KEY,
+  name category_type NOT NULL,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.product (
-    id serial PRIMARY KEY NOT NULL,
-    product_name varchar(90) UNIQUE NOT NULL,
-    product_description varchar(255),
-    price numeric(10, 2) NOT NULL,
-    quantity int,
-    image_url VARCHAR(255), 
-    stars INT, 
-    product_flavor int,
-    FOREIGN KEY (product_flavor) REFERENCES public.flavor(id)
+CREATE TABLE Restaurant_RestaurantCategory (
+  restaurant_id INT REFERENCES Restaurant(id),
+  category_id INT REFERENCES Restaurant_Category(id),
+  PRIMARY KEY (restaurant_id, category_id),
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.orders (  
-    id serial PRIMARY KEY NOT NULL,
-    payment public.payment,
-    order_size public.order_size,
-    order_state public.order_state,
-    must_deliver boolean NOT NULL,
-    order_time timestamp NOT NULL,
-    delivery_time timestamp,
-    priceTotal numeric(10, 2) NOT NULL,
-    delivery_people int,
-    client int,
-    employ int,
-    FOREIGN KEY (delivery_people) REFERENCES public.delivery_people(id),
-    FOREIGN KEY (client) REFERENCES public.client(id),
-    FOREIGN KEY (employ) REFERENCES public.employ(id)
+CREATE TABLE Restaurant_Hours (
+  id SERIAL PRIMARY KEY,
+  restaurant_id INT REFERENCES Restaurant(id),
+  day_of_week day_of_week NOT NULL,
+  open_time TIME,
+  close_time TIME,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.order_items (
-    order_id int NOT NULL,
-    product_id int NOT NULL,
-    quantity int NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES public.orders(id),
-    FOREIGN KEY (product_id) REFERENCES public.product(id),
-    PRIMARY KEY(order_id, product_id)
+CREATE TABLE Restaurant_Image (
+  id SERIAL PRIMARY KEY,
+  restaurant_id INT REFERENCES Restaurant(id),
+  image_url VARCHAR(255) NOT NULL,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.reviews (
-    id serial PRIMARY KEY NOT NULL,
-    client_id int NOT NULL,
-    product_id int,
-    order_id int,
-    rating int CHECK(rating BETWEEN 1 AND 5),
-    comment varchar(255),
-    review_time timestamp NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (client_id) REFERENCES public.client(id),
-    FOREIGN KEY (product_id) REFERENCES public.product(id),
-    FOREIGN KEY (order_id) REFERENCES public.orders(id)
+CREATE TABLE Product (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  price FLOAT NOT NULL,
+  category_id INT REFERENCES Product_Category(id),
+  available BOOLEAN,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
-CREATE TABLE public.report (
-    id serial PRIMARY KEY NOT NULL,
-    report_time timestamp NOT NULL,
-    report_text varchar(255) NOT NULL
+CREATE TABLE Product_Image (
+  id SERIAL PRIMARY KEY,
+  product_id INT REFERENCES Product(id),
+  image_url VARCHAR(255) NOT NULL,
+  status status_type NOT NULL DEFAULT 'ATIVO'
 );
 
--- Clear all values from tables
-TRUNCATE TABLE public.report, public.order, public.product, public.flavor, public.delivery_people, public.address, public.client, public.employ, public.register_user RESTART IDENTITY;
+CREATE TABLE "order" (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES "user"(id),
+  payment_id INT,
+  delivery_address_id INT REFERENCES Address(id),
+  order_time TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- users
-INSERT INTO public.register_user (name, email, password, salt)
-VALUES 
-    ('John Doe', 'john@example.com', 'hashed_password_123', 'some_salt_123'),
-    ('Jane Smith', 'jane@example.com', 'hashed_password_456', 'some_salt_456'),
-    ('Michael Brown', 'michael@example.com', 'hashed_password_789', 'some_salt_789');
+CREATE TABLE Order_Status (
+  id SERIAL PRIMARY KEY,
+  order_id INT REFERENCES "order"(id),
+  processing_status processing_status,
+  status_time TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- employee
-INSERT INTO public.employ (user_id, cpf, name, phone, permission, salary)
-VALUES
-    (1, '1234567890', 'Employee 1', '123456790', 'employee', 25000),
-    (2, '9876543210', 'Employee 2', '987654210', 'manager', 40000);
+CREATE TABLE Order_Item (
+  id SERIAL PRIMARY KEY,
+  order_id INT REFERENCES "order"(id),
+  product_id INT REFERENCES Product(id),
+  quantity INT,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- client
-INSERT INTO public.client (user_id, address_id, cpf, name, phone)
-VALUES
-    (1, 1, '11111111111', 'Client 1', '111111111'),
-    (2, 2, '22222222222', 'Client 2', '222222222');
+CREATE TABLE Review (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES "user"(id),
+  rating INT,
+  comment TEXT,
+  reviewable_id INT,
+  reviewable_type reviewable_type,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- addresses
-INSERT INTO public.address (streetName, streetNum, city, state, postal_code)
-VALUES
-    ('Main Street', 123, 'City1', 'State1', '12345'),
-    ('Second Avenue', 456, 'City2', 'State2', '67890');
+CREATE TABLE Delivery_Person (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(20),
+  vehicle_type vehicle_type,
+  latitude FLOAT,
+  longitude FLOAT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- deliveryP
-INSERT INTO public.delivery_people (employ_id, cpf, name, phone)
-VALUES
-    (1, '33333333333', 'Delivery Person 1', '333333333'),
-    (2, '44444444444', 'Delivery Person 2', '444444444');
+CREATE TABLE Delivery (
+  id SERIAL PRIMARY KEY,
+  delivery_person_id INT REFERENCES Delivery_Person(id),
+  order_id INT REFERENCES "order"(id),
+  delivery_time TIMESTAMP,
+  processing_status processing_status,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- flavor
-INSERT INTO public.flavor (flavor_name, flavor_price, flavor_ingredients)
-VALUES
-    ('Margherita', 9.99, 'Tomato sauce, mozzarella cheese, basil'),
-    ('Pepperoni', 11.99, 'Tomato sauce, mozzarella cheese, pepperoni'),
-    ('Vegetarian', 10.49, 'Tomato sauce, mozzarella cheese, mushrooms, onions, bell peppers');
+CREATE TABLE Payment (
+  id SERIAL PRIMARY KEY,
+  order_id INT REFERENCES "order"(id),
+  amount FLOAT NOT NULL,
+  payment_method_id INT REFERENCES Payment_Method(id),
+  card_id INT REFERENCES Card(id),
+  processing_status processing_status,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
---  product 
-INSERT INTO public.product (product_name, product_description, price, quantity, product_flavor, image_url, stars)
-VALUES
-    ('Pizza', 'Traditional Italian Margherita pizza', 9.99, 50, 1, 'https://raw.githubusercontent.com/Guilherme-Denarde/Trabalhos_Facu/images/licensed-image.jpg', 5),
-    ('Pizza Pepperoni', 'Delicious pepperoni pizza with a spicy kick', 11.99, 40, 1, 'https://raw.githubusercontent.com/Guilherme-Denarde/Trabalhos_Facu/images/licensed-image.jpg', 4),
-    ('Pizza Veggie', 'Healthy and flavorful vegetarian pizza', 10.49, 30, 1, 'https://raw.githubusercontent.com/Guilherme-Denarde/Trabalhos_Facu/images/licensed-image.jpg', 4);
+CREATE TABLE Order_Promotion (
+  order_id INT REFERENCES "order"(id),
+  promotion_id INT REFERENCES Promotion(id),
+  PRIMARY KEY (order_id, promotion_id),
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- order 
-INSERT INTO public.orders (payment, order_size, order_state, must_deliver, order_time, priceTotal, delivery_person_id, client_id, employ_id)
-VALUES
-    ('card', 'm', 'open', true, NOW(), 20.00, 1, 1, 1),
-    ('pix', 'g', 'making', false, NOW(), 50.00, 2, 2, 2),
-    ('money', 'p', 'finished', false, NOW(), 30.00, 1, 1, 1);
+CREATE TABLE Restaurant_BankAccount (
+  id SERIAL PRIMARY KEY,
+  restaurant_id INT REFERENCES Restaurant(id),
+  bank_name VARCHAR(255) NOT NULL,
+  account_number VARCHAR(255) NOT NULL,
+  agency_number VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status status_type NOT NULL DEFAULT 'ATIVO'
+);
 
--- order_items
-INSERT INTO public.order_items (order_id, product_id, quantity)
-VALUES
-    (1, 1, 2), 
-    (2, 2, 5), 
-    (3, 2, 3);
-
--- Select Statments
-SELECT * FROM public.register_user;
-SELECT * FROM public.employ;
-SELECT * FROM public.client;
-SELECT * FROM public.address;
-SELECT * FROM public.delivery_people;
-SELECT * FROM public.flavor;
-SELECT * FROM public.product;
-SELECT * FROM public.orders;
-SELECT * FROM public.order_items;
-SELECT * FROM public.reviews;
-SELECT * FROM public.report;
+-- Atualizar a tabela Order para adicionar a FK de Payment
+ALTER TABLE "order" ADD FOREIGN KEY (payment_id) REFERENCES Payment(id);
