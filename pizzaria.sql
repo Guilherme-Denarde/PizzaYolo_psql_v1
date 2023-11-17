a06e63044b8686ba9b60a07355463308789c2728
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2023-11-17 14:05:20 -0300

    Update pizzaria.sql

--- pizzaria.sql
+++ pizzaria.sql
@@ -1,250 +1,195 @@
-CREATE TYPE payment_method_type AS ENUM ('PIX', 'CARD', 'MONEY');
-CREATE TYPE roles AS ENUM ('ADMIN', 'USER', 'RESTAURANT');
-CREATE TYPE processing_status AS ENUM ('PROCESSANDO', 'A_CAMINHO', 'ENTREGUE', 'ERRO');
-CREATE TYPE status_type AS ENUM ('ATIVO', 'INATIVO');
-CREATE TYPE vehicle_type AS ENUM ('BICICLETA', 'MOTO', 'CARRO');
-CREATE TYPE day_of_week AS ENUM ('DOMINGO', 'SEGUNDA', 'TERÇA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO');
-CREATE TYPE reviewable_type AS ENUM ('PRODUTO', 'RESTAURANTE');
-CREATE TYPE category_type AS ENUM (
-    'BRASILEIRA', 
-    'ITALIANA', 
-    'JAPONESA', 
-    'CHINESA', 
-    'FAST_FOOD', 
-    'VEGETARIANA', 
-    'VEGANA', 
-    'SOBREMESAS', 
-    'LANCHES', 
-    'PIZZA', 
-    'MARISQUEIRA', 
-    'PADARIA', 
-    'CAFETERIA', 
-    'MEXICANA',
-	'BEBIDA'
-);
+-- Enumerators
+CREATE TYPE PaymentMethodEnum AS ENUM ('PIX', 'CARD', 'MONEY');
+CREATE TYPE PaymentStatusEnum AS ENUM ('PENDING', 'COMPLETED', 'FAILED');
+CREATE TYPE RoleEnum AS ENUM ('ADMIN', 'USER', 'RESTAURANT');
+CREATE TYPE ProductSize AS ENUM ('P', 'M', 'G', 'GG');
+CREATE TYPE VehicleType AS ENUM ('BIKE', 'CAR', 'MOTORBIKE');
+CREATE TYPE OrderStatusEnum AS ENUM ('Confirmed', 'Prepared', 'Ready_for_Delivery', 'Completed', 'Cancelled');
+CREATE TYPE DeliveryStatusEnum AS ENUM ('PROCESSING', 'EN_ROUTE', 'DELIVERED');
+CREATE TYPE ProductCategoryEnum AS ENUM ('BRASILEIRA', 'ITALIANA', 'JAPONESA', 'CHINESA', 'FAST_FOOD', 'VEGETARIANA', 'VEGANA', 'SOBREMESAS', 'LANCHES', 'PIZZA', 'MARISQUEIRA', 'PADARIA', 'CAFETERIA', 'MEXICANA', 'BEBIDA');
+CREATE TYPE DayOfWeekEnum AS ENUM ('SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY');
 
-
-CREATE TABLE Role (
+-- Tables
+CREATE TABLE "Role" (
   role_id SERIAL PRIMARY KEY,
-  role_name roles NOT NULL
+  role_name RoleEnum
 );
 
-CREATE TABLE Product_Category (
+CREATE TABLE "User" (
   id SERIAL PRIMARY KEY,
-  name category_type NOT NULL,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE Payment_Method (
-  id SERIAL PRIMARY KEY,
-  method_name payment_method_type NOT NULL,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE Promotion (
-  id SERIAL PRIMARY KEY,
-  description TEXT,
-  discount_rate FLOAT,
-  start_date DATE,
-  end_date DATE,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE "user" (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255),
   phone VARCHAR(20),
   profile_picture VARCHAR(255),
   date_of_birth DATE,
   cpf VARCHAR(20),
-  role_id INT REFERENCES Role(role_id),
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  role_id INT REFERENCES "Role" (role_id)
 );
 
-CREATE TABLE Address (
+CREATE TABLE "Address" (
   id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES "user"(id),
-  streetName VARCHAR(255) NOT NULL,
-  streetNum INT NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  state VARCHAR(255) NOT NULL,
-  country VARCHAR(255) NOT NULL,
+  user_id INT REFERENCES "User" (id),
+  streetName VARCHAR(255),
+  streetNum INT,
+  city VARCHAR(255),
+  state VARCHAR(255),
+  country VARCHAR(255),
   zipcode VARCHAR(20),
   additional_info TEXT,
-  is_default BOOLEAN,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  is_default BOOLEAN
 );
 
-CREATE TABLE Card (
+CREATE TABLE "PaymentMethod" (
   id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES "user"(id),
-  card_number VARCHAR(255) NOT NULL,
-  cardholder_name VARCHAR(255) NOT NULL,
+  method_name PaymentMethodEnum
+);
+
+CREATE TABLE "Card" (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES "User" (id),
+  card_number VARCHAR(255),
+  cardholder_name VARCHAR(255),
   expiry_date DATE,
-  cvv VARCHAR(4),
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  cvv VARCHAR(4)
 );
 
-CREATE TABLE Restaurant (
+CREATE TABLE "Payment" (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  address_id INT REFERENCES Address(id),
+  order_id INT, -- FK Set later due to dependency
+  amount FLOAT,
+  payment_method_id INT REFERENCES "PaymentMethod" (id),
+  card_id INT REFERENCES "Card" (id),
+  status PaymentStatusEnum
+);
+
+CREATE TABLE "ProductCategory" (
+  id SERIAL PRIMARY KEY,
+  name ProductCategoryEnum
+);
+
+CREATE TABLE "Product" (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  price FLOAT,
+  category_id INT REFERENCES "ProductCategory" (id),
+  available BOOLEAN,
+  discount BOOLEAN,
+  discount_price FLOAT,
+  size ProductSize
+);
+
+CREATE TABLE "ProductImage" (
+  id SERIAL PRIMARY KEY,
+  product_id INT REFERENCES "Product" (id),
+  image_url VARCHAR(255)
+);
+
+CREATE TABLE "Order" (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES "User" (id),
+  payment_id INT REFERENCES "Payment" (id),
+  delivery_address_id INT REFERENCES "Address" (id),
+  order_date DATE,
+  order_time TIMESTAMP,
+  total_price FLOAT,
+  requires_delivery BOOLEAN,
+  status OrderStatusEnum
+);
+
+CREATE TABLE "OrderStatus" (
+  id SERIAL PRIMARY KEY,
+  order_id INT REFERENCES "Order" (id),
+  status OrderStatusEnum,
+  status_time TIMESTAMP
+);
+
+CREATE TABLE "OrderItem" (
+  id SERIAL PRIMARY KEY,
+  order_id INT REFERENCES "Order" (id),
+  product_id INT REFERENCES "Product" (id),
+  quantity INT
+);
+
+CREATE TABLE "Restaurant" (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  address_id INT REFERENCES "Address" (id),
   email VARCHAR(255),
   phone VARCHAR(20),
   description TEXT,
-  rating_average FLOAT,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  rating_average FLOAT
 );
 
-CREATE TABLE Restaurant_Category (
+CREATE TABLE "RestaurantCategory" (
   id SERIAL PRIMARY KEY,
-  name category_type NOT NULL,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  name VARCHAR(255)
 );
 
-CREATE TABLE Restaurant_RestaurantCategory (
-  restaurant_id INT REFERENCES Restaurant(id),
-  category_id INT REFERENCES Restaurant_Category(id),
-  PRIMARY KEY (restaurant_id, category_id),
-  status status_type NOT NULL DEFAULT 'ATIVO'
+CREATE TABLE "Restaurant_RestaurantCategory" (
+  restaurant_id INT REFERENCES "Restaurant" (id),
+  category_id INT REFERENCES "RestaurantCategory" (id),
+  PRIMARY KEY (restaurant_id, category_id)
 );
 
-CREATE TABLE Restaurant_Hours (
+CREATE TABLE "RestaurantImage" (
   id SERIAL PRIMARY KEY,
-  restaurant_id INT REFERENCES Restaurant(id),
-  day_of_week day_of_week NOT NULL,
+  restaurant_id INT REFERENCES "Restaurant" (id),
+  image_url VARCHAR(255)
+);
+
+CREATE TABLE "RestaurantHours" (
+  id SERIAL PRIMARY KEY,
+  restaurant_id INT REFERENCES "Restaurant" (id),
+  day_of_week DayOfWeekEnum,
   open_time TIME,
-  close_time TIME,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  close_time TIME
 );
 
-CREATE TABLE Restaurant_Image (
+CREATE TABLE "Review" (
   id SERIAL PRIMARY KEY,
-  restaurant_id INT REFERENCES Restaurant(id),
-  image_url VARCHAR(255) NOT NULL,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE Product (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
-  price FLOAT NOT NULL,
-  category_id INT REFERENCES Product_Category(id),
-  available BOOLEAN,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE Product_Image (
-  id SERIAL PRIMARY KEY,
-  product_id INT REFERENCES Product(id),
-  image_url VARCHAR(255) NOT NULL,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE "order" (
-  id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES "user"(id),
-  payment_id INT,
-  delivery_address_id INT REFERENCES Address(id),
-  order_time TIMESTAMP,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE Order_Status (
-  id SERIAL PRIMARY KEY,
-  order_id INT REFERENCES "order"(id),
-  processing_status processing_status,
-  status_time TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE Order_Item (
-  id SERIAL PRIMARY KEY,
-  order_id INT REFERENCES "order"(id),
-  product_id INT REFERENCES Product(id),
-  quantity INT,
-  status status_type NOT NULL DEFAULT 'ATIVO'
-);
-
-CREATE TABLE Review (
-  id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES "user"(id),
+  user_id INT REFERENCES "User" (id),
+  restaurant_id INT REFERENCES "Restaurant" (id),
   rating INT,
-  comment TEXT,
-  reviewable_id INT,
-  reviewable_type reviewable_type,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  comment TEXT
 );
 
-CREATE TABLE Delivery_Person (
+CREATE TABLE "DeliveryPerson" (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
   phone VARCHAR(20),
-  vehicle_type vehicle_type,
+  vehicle_type VehicleType,
   latitude FLOAT,
-  longitude FLOAT,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  longitude FLOAT
 );
 
-CREATE TABLE Delivery (
+CREATE TABLE "Delivery" (
   id SERIAL PRIMARY KEY,
-  delivery_person_id INT REFERENCES Delivery_Person(id),
-  order_id INT REFERENCES "order"(id),
+  delivery_person_id INT REFERENCES "DeliveryPerson" (id),
+  order_id INT REFERENCES "Order" (id),
   delivery_time TIMESTAMP,
-  processing_status processing_status,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  status DeliveryStatusEnum
 );
 
-CREATE TABLE Payment (
+CREATE TABLE "Voucher" (
   id SERIAL PRIMARY KEY,
-  order_id INT REFERENCES "order"(id),
-  amount FLOAT NOT NULL,
-  payment_method_id INT REFERENCES Payment_Method(id),
-  card_id INT REFERENCES Card(id),
-  processing_status processing_status,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  description TEXT,
+  discount_rate FLOAT,
+  start_date DATE,
+  end_date DATE
 );
 
-CREATE TABLE Order_Promotion (
-  order_id INT REFERENCES "order"(id),
-  promotion_id INT REFERENCES Promotion(id),
-  PRIMARY KEY (order_id, promotion_id),
-  status status_type NOT NULL DEFAULT 'ATIVO'
+CREATE TABLE "Order_Voucher" (
+  order_id INT REFERENCES "Order" (id),
+  voucher_id INT REFERENCES "Voucher" (id),
+  PRIMARY KEY (order_id, voucher_id)
 );
 
-CREATE TABLE Restaurant_BankAccount (
+CREATE TABLE "BankAccount" (
   id SERIAL PRIMARY KEY,
-  restaurant_id INT REFERENCES Restaurant(id),
-  bank_name VARCHAR(255) NOT NULL,
-  account_number VARCHAR(255) NOT NULL,
-  agency_number VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status status_type NOT NULL DEFAULT 'ATIVO'
+  restaurant_id INT REFERENCES "Restaurant" (id),
+  bank_name VARCHAR(255),
+  account_number VARCHAR(255),
+  agency_number VARCHAR(255)
 );
-
--- Atualizar a tabela Order para adicionar a FK de Payment
-ALTER TABLE "order" ADD FOREIGN KEY (payment_id) REFERENCES Payment(id);
